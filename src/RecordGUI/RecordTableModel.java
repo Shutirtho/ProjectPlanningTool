b25d23c1c95ac276906a4e3744f8e5411c90be5f
Shutirtho <sdatta@scu.edu>
2015-07-13 07:54:19 -0700

    Update RecordTableModel.java

--- src/RecordGUI/RecordTableModel.java
+++ src/RecordGUI/RecordTableModel.java
@@ -12,8 +12,6 @@ import javax.swing.table.AbstractTableModel;
  * Updated model Model for the courselist table. Contains the database-specific
  * code to read the data from the database into the table model.
  * 
- * @author Kevin
- * 
  * 
  */
 
